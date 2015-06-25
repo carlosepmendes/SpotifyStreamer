667fb381646aea8620fb40bfed126dcf76325b74
Carlos Mendes <carlosepmendes@gmail.com>
2015-06-25 00:28:35 +0000

    Added Art class to save query results

--- app/src/main/java/com/example/android/spotifystreamer/FetchArtists.java
+++ app/src/main/java/com/example/android/spotifystreamer/FetchArtists.java
@@ -3,44 +3,70 @@ package com.example.android.spotifystreamer;
 import android.os.AsyncTask;
 import android.util.Log;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
 import kaaes.spotify.webapi.android.models.ArtistsPager;
 
 
 /**
  * Created by Carlos on 23/06/2015.
  */
-public class FetchArtists extends AsyncTask <String, Void, ArtistsPager>{
+public class FetchArtists extends AsyncTask <String, Void, ArrayList>{
 
     private final String LOG_TAG = FetchArtists.class.getSimpleName() ;
 
     @Override
-    protected ArtistsPager doInBackground(String... params) {
+    protected ArrayList doInBackground(String... params) {
 
         try{
             SpotifyApi api = new SpotifyApi();
             SpotifyService spotify = api.getService();
             ArtistsPager results = spotify.searchArtists(params[0]);
-            System.out.println(results);
-            System.out.println("--------------------------------------------------------");
-            System.out.println("--------------------------------------------------------");
-            System.out.println("--------------------------------------------------------");
-            System.out.println("--------------------------------------------------------");
-            return results;
+
+            ArrayList<Art> list = new ArrayList<Art>();
+            Iterator<Artist> iterator = results.artists.items.iterator();
+            while (iterator.hasNext()) {
+
+                Artist ar = iterator.next();
+
+                list.add(new Art(ar.id,ar.name));
+
+            }
+
+            return list;
+
 
         }catch(Exception e){
             Log.e(LOG_TAG, "Error ", e);
-            // If the code didn't successfully get the weather data, there's no point in attemping
+            // If the code didn't successfully get the artists data, there's no point in attemping
             // to parse it.
             return null;
         }finally{
-
+            return null;
         }
 
-
-
     }
+
+
+    class Art {
+        public String id;
+        public String name;
+        public String photo;
+
+        public Art(String id, String name){
+            this.id = id;
+            this.name = name;
+            //this.photo = photo;
+        }
+    }
+
+
+
+
 }
+
--- app/src/main/java/com/example/android/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/com/example/android/spotifystreamer/MainActivityFragment.java
@@ -7,13 +7,18 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
-
+    
     public MainActivityFragment() {
     }
 
@@ -28,5 +33,6 @@ public class MainActivityFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_main, container, false);
 
     }
+
 
 }
