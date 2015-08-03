d13a7340c6dbdf30494c78419a48c0c9d2b280a0
CarlosMendes <carlosepmendes@gmail.com>
2015-08-03 22:14:11 +0000

    Changed Song and Band Classes to parcelables

--- app/src/main/java/com/example/android/spotifystreamer/Band.java
+++ app/src/main/java/com/example/android/spotifystreamer/Band.java
@@ -1,17 +1,62 @@
 package com.example.android.spotifystreamer;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Carlos on 29/06/2015.
  * This class will model our query results
  */
-public class Band {
+public class Band implements Parcelable{
     public String id;
     public String name;
     public String photo;
 
-    public Band(String id, String name, String photo){
-        this.id = id;
-        this.name = name;
+    public Band(String photo, String name, String id) {
         this.photo = photo;
+        this.name = name;
+        this.id = id;
     }
+
+    private Band (Parcel in){
+        id = in.readString();
+        name = in.readString();
+        photo = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Band{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", photo='" + photo + '\'' +
+                '}';
+    }
+
+    /**
+     // Storing the Band data to a Parcel object
+     **/
+    @Override
+    public void writeToParcel(Parcel parcel, int flags) {
+        parcel.writeString(photo);
+        parcel.writeString(name);
+        parcel.writeString(id);
+    }
+
+    public final Parcelable.Creator<Band> CREATOR = new Creator<Band>() {
+        @Override
+        public Band createFromParcel(Parcel source) {
+            return new Band(source);
+        }
+
+        @Override
+        public Band[] newArray(int size) {
+            return new Band[size];
+        }
+    };
 }
--- app/src/main/java/com/example/android/spotifystreamer/Song.java
+++ app/src/main/java/com/example/android/spotifystreamer/Song.java
@@ -1,11 +1,12 @@
 package com.example.android.spotifystreamer;
 
-import java.net.URL;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 /**
  * Created by Carlos on 30/06/2015.
  */
-public class Song {
+public class Song implements Parcelable{
     public String albumName;
     public String name;
     public String photoSmall;
@@ -21,4 +22,46 @@ public class Song {
         this.previewUrl = previewUrl;
     }
 
+    public Song(Parcel source) {
+        albumName = source.readString();
+        name = source.readString();
+        photoSmall = source.readString();
+        photoLarge = source.readString();
+        previewUrl = source.readString();
+    }
+
+    @Override
+    public int describeContents() { return 0; }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "albumName='" + albumName + '\'' +
+                ", name='" + name + '\'' +
+                ", photoSmall='" + photoSmall + '\'' +
+                ", photoLarge='" + photoLarge + '\'' +
+                ", previewUrl='" + previewUrl + '\'' +
+                '}';
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int flags) {
+        parcel.writeString(albumName);
+        parcel.writeString(name);
+        parcel.writeString(photoSmall);
+        parcel.writeString(photoLarge);
+        parcel.writeString(previewUrl);
+    }
+
+    public final Parcelable.Creator<Song> CREATOR = new Creator<Song>() {
+        @Override
+        public Song createFromParcel(Parcel source) {
+            return new  Song(source);
+        }
+
+        @Override
+        public Song[] newArray(int size) {
+            return new Song[size];
+        }
+    };
 }
