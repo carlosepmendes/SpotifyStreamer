2354925f02a7d59cd42832010586bf0110581b62
carlosepmendes <carlosepmendes@gmail.com>
2015-08-17 14:24:51 +0000

    Added get parent activity intent to fix the back menu button

--- app/src/main/java/com/example/android/spotifystreamer/PlayerActivity.java
+++ app/src/main/java/com/example/android/spotifystreamer/PlayerActivity.java
@@ -1,5 +1,8 @@
 package com.example.android.spotifystreamer;
 
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,6 +16,11 @@ public class PlayerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_player);
     }
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public Intent getParentActivityIntent() {
+        return super.getParentActivityIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/example/android/spotifystreamer/SongActivity.java
+++ app/src/main/java/com/example/android/spotifystreamer/SongActivity.java
@@ -1,5 +1,9 @@
 package com.example.android.spotifystreamer;
 
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -14,6 +18,11 @@ public class SongActivity extends ActionBarActivity {
         setContentView(R.layout.activity_song);
     }
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public Intent getParentActivityIntent() {
+        return super.getParentActivityIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
